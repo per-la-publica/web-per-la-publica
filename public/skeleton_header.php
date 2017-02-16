70859ea5eaf93a5bdf30ef8af34d4a55f6706590
Oriol Teixidó <oriol.teixido@gmail>
2017-02-16 23:42:18 +0100

    Ocultar enllaç a funcionament

--- public/skeleton_header.php
+++ public/skeleton_header.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Per la Pública">
     <meta name="author" content="Per la Pública">
-    
+
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
@@ -41,35 +41,35 @@
         <div class="collapse navbar-collapse">
         <nav>
           <ul class="nav navbar-nav">
-			<li><a href="carta.html" title="Carta de presentació">Presentació</a></li>
-			<li><a href="ideari.html" title="El nostre ideari">Ideari</a></li>
-			<li><a href="manifest.html" title="El Manifest de Per la Pública">Manifest</a></li>
-			<li><a href="funcionament.html" title="La nostra manera de fer">Funcionament</a></li>
-			<li><a href="membres_claustre.html" title="Grup de treball del Claustre">Claustre</a></li>
-			<li><a href="activitat.html" title="Agenda d'activitats">Activitats</a></li>
-			<?php /*<li><a href="participa.html">Participa</a></li>*/ ?>
-			<li><a href="contacta.html" title="Posa't en contacte amb nosaltres">Contacta</a></li>          
-		  </ul>
+            <li><a href="carta.html" title="Carta de presentació">Presentació</a></li>
+            <li><a href="ideari.html" title="El nostre ideari">Ideari</a></li>
+            <li><a href="manifest.html" title="El Manifest de Per la Pública">Manifest</a></li>
+            <?php /*<li><a href="funcionament.html" title="La nostra manera de fer">Funcionament</a></li>*/ ?>
+            <li><a href="membres_claustre.html" title="Grup de treball del Claustre">Claustre</a></li>
+            <li><a href="activitat.html" title="Agenda d'activitats">Activitats</a></li>
+            <?php /*<li><a href="participa.html">Participa</a></li>*/ ?>
+            <li><a href="contacta.html" title="Posa't en contacte amb nosaltres">Contacta</a></li>
+          </ul>
         </div><!--/.nav-collapse -->
         </nav>
       </div>
     </div>
 
-	<?php if (!empty($titol)) : ?>
-		<div class="jumbotron subhead" id="overview">
-  			<div class="container">
-				<h1><?php echo $titol; ?></h1>
-			</div>
-		</div>
-	<?php endif; ?>
+    <?php if (!empty($titol)) : ?>
+        <div class="jumbotron subhead" id="overview">
+            <div class="container">
+                <h1><?php echo $titol; ?></h1>
+            </div>
+        </div>
+    <?php endif; ?>
 
-<?php 
+<?php
 /*
 	<div class="xarxes-socials"><?php require_once('skeleton_xarxes_socials.php'); ?></div>
-*/	
+*/
 ?>
-	<?php if (!isset($paginaPrincipal) || !$paginaPrincipal) : ?>
+    <?php if (!isset($paginaPrincipal) || !$paginaPrincipal) : ?>
         <div class="container">
-       	    <div class="row">		
-        		<div class="col-md-12">
-	<?php endif; ?>
+            <div class="row">
+                <div class="col-md-12">
+    <?php endif; ?>
