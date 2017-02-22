b79efe3eb6574cbf9a3cae8d6fcf3d1ac79d86f9
Oriol Teixidó <oriol.teixido@gmail.com>
2017-02-22 08:15:45 +0100

    Afegir descripció a la pàgina d'històric

--- public/historic.php
+++ public/historic.php
@@ -1,18 +1,19 @@
-<?php	
+<?php
 	$titol = 'Ideari';
-	require('skeleton_header.php'); 
+	require('skeleton_header.php');
 ?>
 
 <div class="col-md-2 logo-container">
-    <p class="logo aside"">*LP</p>
+    <p class="logo aside">*LP</p>
 </div>
+
 <div class="col-md-8">
-<h2>Històric</h2>
-
+    <h2>Històric</h2>
     <p class="lead">Aquesta pàgina recull l'històric de versions de la nostra web.</p>
-
-    <p><a href="historic/2013/index.php">La nostra web, del 2013 fins al 2016.</a></p>
-
+    <p>Hi podeu consultar tots els documents publicats històricament, les persones que hi han participat o l'evolució de *LP en el temps (funcionament, ideari, ...).</p>
+    <ul>
+        <li><a href="historic/2013/index.php">La nostra web, del 2013 fins al 2016.</a></li>
+    </ul>
 </div>
 
 <?php require('skeleton_footer.php'); ?>
