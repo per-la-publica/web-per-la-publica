c40bf47286645196c9c8ac09ed1eec9f5f94ed95
Angel Aguilera <angel.aguilera@gmail.com>
2017-03-16 23:24:26 +0100

    Afegit esquelet per a la info. de campanya

---
+++ public/info-campanya.php
@@ -0,0 +1,20 @@
+<?php	
+$titol = 'Campanya electoral Claustre 2017';
+require('skeleton_header.php'); 
+?>
+
+<div class="col-md-2 logo-container hidden-md hidden-xs hidden-sm">
+    <p class="logo" style="font-size: 75px; padding-top: 25px">*LP</p>
+</div>
+<div class="col-md-8">
+
+	<article>
+	<h2>Títol</h2>
+	
+	<p>Lorem ipsum...</p>
+	</article>
+	
+</div>
+
+
+<?php require('skeleton_footer.php'); ?>
