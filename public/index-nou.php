f63f53b48809cdc415a001893aab9804cc947c41
Angel Aguilera <angel.aguilera@gmail.com>
2017-03-20 22:48:50 +0100

    Incorporats canvis suggerits a la pag. inicial

--- public/index-nou.php
+++ public/index-nou.php
@@ -1,23 +1,13 @@
 <?php
+    $titol = '<span class="logo">*LP</span> Per la Pública';
 	$paginaPrincipal = true;
 	require('skeleton_header.php');
 ?>
 
-<div class="jumbotron masthead">
-    <div class="container">
-	    <h1><span class="logo">*LP</span> <span style="font-weight:normal;font-size: 65pt">Per la Pública</span></h1>
-    </div>
-</div>
 <div class="container">
-    <div class="row" style="padding-top: 3em">
+    <div class="row">
 
         <div class="col-md-8">
-
-	        <p class="lead">Som un grup de persones de la comunitat UPC compromeses amb la universitat pública
-		       i que volem defensar la seva qualitat i els seus valors.</p>
-	
-			<p class="lead">Entre nosaltres trobareu <a href="membres_claustre.php">representants al Claustre</a>, al Consell
-	               de Govern i al Consell Social de la UPC. Volem fer sentir la vostra veu!</p>
 	
 			<div class="panel panel-info">
 				<div class="panel-heading clearfix">
@@ -31,16 +21,14 @@
 				</div>
 			</div>
 	
-	            <?php
-	            /*
-	             <p class="lead">Us esperem a la <a href="https://comunitat.perlapublica.org/">Comunitat</a>.
-	               Podeu fer les vostres aportacions al
-	               <a href="https://comunitat.perlapublica.org/groups/profile/81/lp">grup «<abbr title="Per la pública">*LP</abbr>»</a>,
-	               el grup obert dins la Comunitat on podeu comentar amb nosaltres tot allò que considereu important.</p>
-	             */
-	            ?>
+	        <p class="lead">Som un grup de persones de la comunitat UPC compromeses amb la universitat pública
+		       i que volem defensar la seva qualitat i els seus valors.</p>
 	
-			<p style="margin-bottom: 50px"><a href="ideari.php" class="btn btn-primary btn-lg" role="button">Consulta el nostre ideari &raquo;</a>
+			<p class="lead">Entre nosaltres trobareu <a href="membres_claustre.php">representants al Claustre</a>, al Consell
+	               de Govern i al Consell Social de la UPC. Volem fer sentir la vostra veu!</p>
+
+			<p style="margin-bottom: 50px">
+			<a href="ideari.php" class="btn btn-primary btn-lg" role="button">Consulta el nostre ideari &raquo;</a>
 			<a href="info-campanya.php" class="btn btn-success btn-lg" role="button">Informació de campanya <span class="glyphicon glyphicon-info-sign"></span></a>
 			<a href="timeline.php" class="btn btn-info btn-lg" role="button">Línia temporal <span class="glyphicon glyphicon-blackboard"></span></a>
 			</p>
