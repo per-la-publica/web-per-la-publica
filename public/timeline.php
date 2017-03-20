1a5932ac0980e8b162e32ae49ecee1a4900faacb
Angel Aguilera <angel.aguilera@gmail.com>
2017-03-20 14:29:24 +0100

    inserció responsiva del iframe segons bootstrap

--- public/timeline.php
+++ public/timeline.php
@@ -5,9 +5,11 @@ require('skeleton_header.php');
 
 <div class="col-md-12">
 
+	<div class="embed-responsive embed-responsive-16by9">
 	<iframe width="100%" height="715" src="timeline/timeline.html" frameborder="0" allowfullscreen>
         <p>Your browser does not support iframes.</p>
     </iframe>
+    </div>
     
 </div>
 
