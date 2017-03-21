70d446738868e14f1a6d013dfd577df419edfdec
Angel Aguilera <angel.aguilera@gmail.com>
2017-03-21 14:10:12 +0100

    Afegit text explicatiu

--- public/timeline.php
+++ public/timeline.php
@@ -5,6 +5,9 @@ require('skeleton_header.php');
 
 <div class="col-md-12">
 
+	<p>Si trobes a faltar alguna cosa, fes-nos-ho arribat al correu 
+	<a href="mailto:upc.per.la.publica@gmail.com">upc.per.la.publica@gmail.com</a>.</p>
+
 	<div class="embed-responsive embed-responsive-16by9">
 	<iframe width="100%" height="715" src="timeline/timeline.html" frameborder="0" allowfullscreen>
         <p>Your browser does not support iframes.</p>
