151e08998d147cbeba27809dd1f75b90e9ad343e
Oriol Teixidó <oriol.teixido@gmail>
2017-06-28 22:25:01 +0200

    Renombrar fitxers de php a html

--- public/historic/2013/skeleton_header.php
+++ public/historic/2013/skeleton_header.php
@@ -36,19 +36,19 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php" style="font-family: Montserrat, sans-serif;font-size: 10pt"><span class="logo">*LP</span> Per la Pública</a>
+          <a class="navbar-brand" href="index.html" style="font-family: Montserrat, sans-serif;font-size: 10pt"><span class="logo">*LP</span> Per la Pública</a>
         </div>
         <div class="collapse navbar-collapse">
         <nav>
           <ul class="nav navbar-nav">
-            <li><a href="carta.php" title="Carta de presentació">Presentació</a></li>
-            <li><a href="ideari.php" title="El nostre ideari">Ideari</a></li>
-            <li><a href="manifest.php" title="El Manifest de Per la Pública">Manifest</a></li>
-            <li><a href="funcionament.php" title="La nostra manera de fer">Funcionament</a></li>
-            <li><a href="membres_claustre.php" title="Grup de treball del Claustre">Claustre</a></li>
-            <li><a href="activitat.php" title="Agenda d'activitats">Activitats</a></li>
-            <li><a href="participa.php">Participa</a></li>
-            <li><a href="contacta.php" title="Posa't en contacte amb nosaltres">Contacta</a></li>
+            <li><a href="carta.html" title="Carta de presentació">Presentació</a></li>
+            <li><a href="ideari.html" title="El nostre ideari">Ideari</a></li>
+            <li><a href="manifest.html" title="El Manifest de Per la Pública">Manifest</a></li>
+            <li><a href="funcionament.html" title="La nostra manera de fer">Funcionament</a></li>
+            <li><a href="membres_claustre.html" title="Grup de treball del Claustre">Claustre</a></li>
+            <li><a href="activitat.html" title="Agenda d'activitats">Activitats</a></li>
+            <li><a href="participa.html">Participa</a></li>
+            <li><a href="contacta.html" title="Posa't en contacte amb nosaltres">Contacta</a></li>
           </ul>
         </nav>
         </div><!--/.nav-collapse -->
