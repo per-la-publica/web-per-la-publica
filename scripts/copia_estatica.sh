0543fed8d5246ed703b8f25313b176f72e1fd99e
Oriol Teixidó <oriol.teixido@gmail>
2017-06-28 22:40:20 +0200

    Crear còpia estàtica de la web

--- scripts/copia_estatica.sh
+++ scripts/copia_estatica.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Genera una còpia estàtica de la web en el directori ./xlp
-wget --recursive --page-requisites --relative http://xlp
+rm -rf static
+wget --recursive --page-requisites --relative http://192.168.34.10/index.html --no-host-directories --directory-prefix=static
 
 # Pàgines que s'han de generar explícitament perquè no hi ha
 # cap enllaç a elles.
 #wget --recursive --page-requisites --relative http://xlp/questionari_eleccions_rector.html
-
