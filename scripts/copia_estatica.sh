6ae8cc71903839f37c4993498022b927256b2412
Oriol Teixidó <oriol.teixido@gmail.com>
2017-06-29 07:08:31 +0200

    Afegir les imatges a l'script de creació de la web estàtica

--- scripts/copia_estatica.sh
+++ scripts/copia_estatica.sh
@@ -2,7 +2,8 @@
 
 # Genera una còpia estàtica de la web en el directori ./xlp
 rm -rf static
-wget --recursive --page-requisites --relative http://192.168.34.10/index.html --no-host-directories --directory-prefix=static
+wget --recursive --no-host-directories --page-requisites --directory-prefix=static http://192.168.34.10/index.html
+echo -n "www.perlapublica.org" > static/CNAME
 
 # Pàgines que s'han de generar explícitament perquè no hi ha
 # cap enllaç a elles.
