2bf574d45e086f30bb5456bb3deb78eeab89d20c
Oriol Teixidó <oriol.teixido@gmail.com>
2017-06-29 07:16:58 +0200

    Afegir les imatges no enllaçades a l'script de creació de la web estàtica

--- scripts/copia_estatica.sh
+++ scripts/copia_estatica.sh
@@ -3,6 +3,8 @@
 # Genera una còpia estàtica de la web en el directori ./xlp
 rm -rf static
 wget --recursive --no-host-directories --page-requisites --directory-prefix=static http://192.168.34.10/index.html
+mkdir -p static/img/; cp -R public/img/* static/img/
+mkdir -p static/historic/2013/img/; cp -R public/historic/2013/img/* static/historic/2013/img/
 echo -n "www.perlapublica.org" > static/CNAME
 
 # Pàgines que s'han de generar explícitament perquè no hi ha
