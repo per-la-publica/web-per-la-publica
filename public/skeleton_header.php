99785ec9f55ee2284fd9ad521baafffc6fff5682
Angel Aguilera <angel.aguilera@gmail.com>
2021-03-19 12:19:41 +0100

    Actualitza referencies externes JS i CSS i plantilla jumbotron

--- public/skeleton_header.php
+++ public/skeleton_header.php
@@ -6,69 +6,67 @@
  *  - $paginaPrincipal(boolean): indica si es tracta de la pàgina principal
  */
 ?>
+
 <head>
-    <meta charset="utf-8">
-    <title>Per la Pública</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Per la Pública">
-    <meta name="author" content="Per la Pública">
+  <meta charset="utf-8">
+  <title>Per la Pública</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Per la Pública">
+  <meta name="author" content="Per la Pública">
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-    <link href="//fonts.googleapis.com/css?family=Varela+Round|Montserrat" rel="stylesheet" type="text/css">
-    <link href="css/xlp.css" rel="stylesheet">
+  <style>
+    @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css");
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Varela+Round&display=swap');
+  </style> 
 
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+  <link href="css/xlp.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="navbar navbar-inverse navbar-fixed-top">
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+  <div class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="index.html" style="font-family: Montserrat, sans-serif;font-size: 10pt"><span class="logo">*LP</span> Per la Pública</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-links" aria-controls="navbar-links" aria-expanded="false" aria-label="Commuta navegació">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbar-links">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="carta.html" title="Carta de presentació">Presentació</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="membres_claustre.html" title="Grup de treball del Claustre">Qui som?</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="activitat.html" title="Agenda d'activitats">Activitats</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="timeline.html" title="Cronologia">Cronologia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contacta.html" title="Posa't en contacte amb nosaltres">Contacta</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+<main role="main">
+
+  <?php if (!empty($titol)) : ?>
+    <div class="jumbotron jumbotron-fluid">
       <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="index.html" style="font-family: Montserrat, sans-serif;font-size: 10pt"><span class="logo">*LP</span> Per la Pública</a>
-        </div>
-        <div class="collapse navbar-collapse">
-        <nav>
-          <ul class="nav navbar-nav">
-            <li><a href="carta.html" title="Carta de presentació">Presentació</a></li>
-            <?php /*<li><a href="funcionament.html" title="La nostra manera de fer">Funcionament</a></li>*/ ?>
-            <li><a href="membres_claustre.html" title="Grup de treball del Claustre">Qui som?</a></li>
-            <li><a href="activitat.html" title="Agenda d'activitats">Activitats</a></li>
-            <?php /*<li><a href="participa.html">Participa</a></li>*/ ?>
-            <li><a href="timeline.html" title="Cronologia">Cronologia</a></li>
-            <li><a href="contacta.html" title="Posa't en contacte amb nosaltres">Contacta</a></li>
-          </ul>
-        </nav>
-        </div><!--/.nav-collapse -->
+        <h1 class="display-4"><?php echo $titol; ?></h1>
       </div>
     </div>
+  <?php endif; ?>
 
-    <?php if (!empty($titol)) : ?>
-        <div class="jumbotron subhead" id="overview">
-            <div class="container">
-                <h1><?php echo $titol; ?></h1>
-            </div>
-        </div>
-    <?php endif; ?>
-
-<?php
-/*
-	<div class="xarxes-socials"><?php require_once('skeleton_xarxes_socials.php'); ?></div>
-*/
-?>
-    <?php if (!isset($paginaPrincipal) || !$paginaPrincipal) : ?>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-    <?php endif; ?>
+  <?php if (!isset($paginaPrincipal) || !$paginaPrincipal) : ?>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+  <?php endif; ?>
